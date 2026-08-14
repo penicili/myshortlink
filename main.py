bee73ab0d2b429862ddce208d05f101b8d651c49
penicili <bagaskoro55@gmail.com>
2026-08-14 20:33:09 +0700

    feat: basic endpoints with placeholder logics

--- main.py
+++ main.py
@@ -1,6 +1,28 @@
-def main():
-    print("Hello from myshortlink!")
+from fastapi import FastAPI
+from pydantic import BaseModel
 
+app = FastAPI()
 
-if __name__ == "__main__":
-    main()
+# Health check
+@app.get('/')
+def healthcheck():
+    return {'OK'}
+
+# Shorten link
+@app.post('/')
+def shorten_link(link):
+    return {"Shortened Link"}
+
+# Open shortlink
+@app.get('/{shortlink}')
+def redirect(shortlink):
+    return {"Redirect to link"}
+
+# Deletes
+@app.delete('/{shortlink}')
+def delete(shortlink):
+    # Cek apakah dia yang buat
+    if (True):
+        return {"Shortlink deleted"}
+    else:
+        return {"No you don't"}
