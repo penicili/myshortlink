1816bb7af3293d1ccca01a3fc28a3c128ce922d7
penicili <bagaskoro55@gmail.com>
2026-08-27 16:48:17 +0700

    modify dockerfile

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ def run() -> None:
     uvicorn.run(
         "app:app",
         app_dir="app",
-        host=os.getenv("APP_HOST", "127.0.0.1"),
+        host="0.0.0.0",
         port=int(os.getenv("APP_PORT", "8000")),
         reload=os.getenv("APP_RELOAD", "true").lower() == "true",
     )
